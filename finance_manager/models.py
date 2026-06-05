3343e4ff9fd610629f580cce8159c2c3f5249059
User-PC <User-PC@DESKTOP-L2C22JJ>
2026-06-05 13:50:16 +0700

    Добавил модели Категорий и Транзакций

--- finance_manager/models.py
+++ finance_manager/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import date
 # Create your models here.
+
+class Category(models.Model):
+    TYPE_CHOICES = [('income', 'Доход'),
+                    ('expense', 'Расход')]
+    name = models.CharField('Название', max_length=50)
+    type = models.CharField('Тип', max_length=10, choices=TYPE_CHOICES)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['name', 'user', 'type']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
+
+class Transaction(models.Model):
+    TYPE_CHOICES = [('income', 'Доход'),
+                    ('expense', 'Расход')]
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='transactions')
+    amount = models.DecimalField('Сумма транзакции', max_digits=10, decimal_places=2)
+    description = models.CharField('Описание транзакции', max_length=250, blank=True)
+    date = models.DateField('Дата транзакции', default=date.today())
+    type = models.CharField('Тип', max_length=10, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date', '-created_at',)
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
