93296a560be009713d72f72f1e5fb5518e963ae1
User-PC <User-PC@DESKTOP-L2C22JJ>
2026-06-05 15:12:12 +0700

    Добавил модели в админ панель

--- finance_manager/admin.py
+++ finance_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from finance_manager.models import Category, Transaction
+
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'user_id', 'created_at')
+    list_filter = ('type', 'created_at')
+    search_fields = ('name', 'user_id')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'type', 'amount', 'created_at')
+    list_filter = ('type', 'created_at')
+    search_field = ('id','user_id')
