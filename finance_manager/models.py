d3405b12d4873cbe70cb14e85c5b33e85031ddcc
User-PC <User-PC@DESKTOP-L2C22JJ>
2026-06-05 15:12:32 +0700

    Добавил строку id в модель Transaction

--- finance_manager/models.py
+++ finance_manager/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import date
+from django.utils import timezone
 # Create your models here.
 
 class Category(models.Model):
@@ -12,7 +12,7 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ['name', 'user', 'type']
+        unique_together = ['name', 'user_id', 'type']
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
@@ -22,12 +22,13 @@ class Category(models.Model):
 class Transaction(models.Model):
     TYPE_CHOICES = [('income', 'Доход'),
                     ('expense', 'Расход')]
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='transactions')
     amount = models.DecimalField('Сумма транзакции', max_digits=10, decimal_places=2)
     description = models.CharField('Описание транзакции', max_length=250, blank=True)
-    date = models.DateField('Дата транзакции', default=date.today())
-    type = models.CharField('Тип', max_length=10, choices=TYPE_CHOICES)
+    date = models.DateField('Дата транзакции', default = timezone.now)
+    type = models.CharField('Тип', max_length=10, choices = TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
